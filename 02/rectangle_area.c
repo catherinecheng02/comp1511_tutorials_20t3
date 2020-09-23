a97cee431d6bd09a9ed9be288ee2f90be9cdc68e
Donna Zhou <13839920+dondonz@users.noreply.github.com>
2020-09-23 15:33:40 +1000

    Add annotations

--- 02/rectangle_area.c
+++ 02/rectangle_area.c
@@ -1,22 +1,26 @@
 // Calculates the area of a rectangle
 // Amazing tutorial W13B, September 2020
+// NOTE: This is a rough working area for the tutorial.
+
+// Exercise: some of the variable names here could be better
+// Your code will have much better style than this :)
 
 #include <stdio.h>
-#define PI 3.14
 #define RECTANGLE_MODE 0
 #define TRIANGLE_MODE 1
+// Remember - no semicolon; after #define
 
 int main(void) {
     // DONE Ask user to type in length, width
+    // Declaring a variable - "I exist!"
     int length;
     int width;
     int mode;
     
-    // double 3.25
-    // Declaring a variable - "I exist!"
-    // Initalising a variable
+    // Initalising a variable with a value.
+    // int number = 0;
     
-    // DONE Read in the information, store it in...??
+    // DONE Read in the information, store it in variables
     printf("Please enter length:");
     scanf("%d", &length);
     printf("Please enter width:");
@@ -26,13 +30,20 @@ int main(void) {
     
     // DONE Calculate the area of a rectangle, length * width
     int area = length * width;
-    
-    // DONE Show the user the area
-    // TODO talk about division
+    // (1/2) produced 0, oh no! Let's divide by a double instead.
     double triangle_area = (1/2.0) * length * width;
     
-    // if (like_cats || like_dogs)
+    // OR operator example
+    // (like_cats || like_dogs) will be true if:
+    // + person likes cats
+    // + person likes dogs
+    // + person likes cats AND dogs
     
+    // DONE Show the user the area
+      
+    // AND operator example
+    // condition will be true if both sides of the AND operator are true:
+    // + mode IS RECTANGLE_MODE AND simultaneously, area greater than or equal to 0
     if (mode == RECTANGLE_MODE && area >= 0) {
         printf("Length: %d Width: %d Rectangle Area = %d\n", length, width, area);
     } else if (mode == TRIANGLE_MODE) {
